3619135f168e2f3cbd6c9c1793f76dab1a62c4c0
Greg Hewett <glh@twoutah.com>
2024-01-28 20:48:29 -0600

    point in time update

--- skeletons/cpp.cpp
+++ skeletons/cpp.cpp
@@ -1,4 +1,37 @@
+/*
+ *  {source}.cpp
+ *
+ *  Copyright (c) {year}
+ *  {name}
+ *  All Rights Reserved
+ *
+ *  {license}
+ *
+ *  {project description}
+ *
+ *  {file description}
+ *
+ */
+
 #include <iostream>
+#include "{header}.hpp"
+
+/*
+ * {namespace}::{object}::{method}
+ *
+ * {method description}
+ *
+ * Params:
+ * {params description}
+ *
+ * Returns:
+ * {return description}
+ *
+ */
+{namesspace}::{object}::{method}() { }
+
+{namesspace}::~{object}::{method}() { }
+
 
 int main(int, char**)
 {
